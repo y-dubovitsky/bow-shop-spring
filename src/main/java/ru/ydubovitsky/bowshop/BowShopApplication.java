98305be1d26c3279f0f20d2c459c4b4590b1326a
user <y.dubovitsky@gmail.com>
2022-02-18 11:02:49 +0300

    Init commit

---
+++ src/main/java/ru/ydubovitsky/bowshop/BowShopApplication.java
@@ -0,0 +1,13 @@
+package ru.ydubovitsky.bowshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ydubovitsky/bowshop/BowShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ydubovitsky.bowshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
